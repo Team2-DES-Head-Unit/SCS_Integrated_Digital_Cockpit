70b6b45e9a3e9c1513704e92556dc2f9058bd0bc
iznue <lej010320@naver.com>
2025-06-06 22:54:08 +0900

    fix error : hu - server

--- HeadUnit/server.h
+++ HeadUnit/server.h
@@ -22,50 +22,12 @@ private:
     struct libxenvchan *vchanServer = nullptr;
 
 public:
-    explicit Server(QObject *parent = nullptr) : QObject(parent), vchanServer(nullptr){
-        // const char *huxs_path = "/local/domain/1/data/hu"; // hu domu id : 1
-        // vchanServer = libxenvchan_server_init(nullptr, 2, huxs_path, 0, 0); // ic domu id : 2
-        // if (!vchanServer){
-        //     qDebug() << "Failed to create vchan server - HU\n";
-        //     return;
-        // } else{
-        //     qDebug() << "HU - IC Vchan server init success\n";
-        // }
-    }
+    explicit Server(QObject *parent = nullptr);
+    ~Server();
 
-    ~Server(){
-        if (vchanServer){
-            libxenvchan_close(vchanServer);
-        }
-    }
+    void start();
 
-    void start(){
-        const char *huxs_path = "/local/domain/1/data/hu"; // hu domu id : 1
-        vchanServer = libxenvchan_server_init(nullptr, 2, huxs_path, 0, 0); // ic domu id : 2
-        if (!vchanServer){
-            qDebug() << "Failed to create vchan server - HU\n";
-            return;
-        } else{
-            qDebug() << "HU - IC Vchan server init success\n";
-        }
-    }
-
-    Q_INVOKABLE void sendModeToIC(int mode){
-        int data = mode;
-
-        int bf_size = libxenvchan_buffer_space(vchanServer);
-        if (bf_size < sizeof(data)) {
-            qDebug() << "Not enough space in vchan buffer.";
-            return;
-        }
-
-        int send_byte = libxenvchan_write(vchanServer, &data, sizeof(data));
-        if (send_byte <= 0){
-            qDebug() << "Failed to write changed mode type\n";
-        } 
-        qDebug() << "Sent mode:" << mode;
-        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
+    Q_INVOKABLE void sendModeToIC(int mode);
 };
 
 #endif // SERVER_H
